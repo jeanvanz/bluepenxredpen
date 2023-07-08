64013e706115615685bfd8335bc84068808731f8
Jean Folle Vanz <jeanfolle86@gmail.com>
2023-07-07 21:56:53 -0300

    Havia cometido equivocos anterirmente no gerasetup, estava como 'include.files' ao invés de 'include_files', também faltou o .icon nesses arquivos inclusos da instalação

--- gerasetup.py
+++ gerasetup.py
@@ -5,17 +5,17 @@ executables=[
 cx_Freeze.setup(
     name='Bluepen x Redpen',
     options={
-        'build.exe':{
+        'build_exe':{
             'packages':['pygame'],
             'include_files':[
                 'bg.png',
                 'bluepen.png',
                 'redpen.png',
-                'cupid.mp3'
+                'cupid.mp3',
+                'bluepen.ico'
             ]
         }
     },executables=executables
 )
-
-#python gerasetup.py build
-#python gerasetup.py bdist_msi
+# python gerasetup.py build
+# python gerasetup.py bdist_msi
